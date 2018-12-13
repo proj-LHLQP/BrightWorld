d8deba482c11d305453a487e916b391aaffc6044
Đặng Ngọc Phong <g1.sem1.t1807e@gmail.com>
2018-12-13 16:31:12 +0700

    Update FontAwesome

---
+++ other/arrayDocument.js
@@ -0,0 +1,59 @@
+[
+	{
+		"prod_id":1,
+		"prod_name":"Đèn âm trần Philips chiếu điểm 5w, 12w, 18w chip COB",
+		"prod_brand":"Philips",
+	 	"prod_power": {
+	 						"power1":"5w",
+	 						"power2":"12w",
+	 						"power3":"18w",
+	 					},
+		"prod_volt": "175 ~ 240 VAC - 50/60 Hz",
+		"prod_colortemp": "3000K|4000K|6500K",
+		"prod_lum_flux": "450/1100/1650 lumen",
+		"prod_dimens":  "(ØxH) D86*H53/D106*H68/D136*H80 mm",
+		"prod_price":  {
+	 						"price1":"158000đ",
+	 						"price2":"395000đ",
+	 						"price3":"525000đ",
+	 					} 
+
+	},
+	{
+		"prod_id":2,
+		"prod_name":"Đèn âm trần Kosoom thân nhựa, 5W - 18W",
+		"prod_brand":"Kosoom",
+	 	"prod_power": {
+	 						"power1":"5w",
+	 						"power2":"18w",
+	 					},
+		"prod_volt": "175 ~ 240 VAC - 50/60 Hz",
+		"prod_colortemp": "3000K|4000K|6500K",
+		"prod_lum_flux": "450/1100/1650 lumen",
+		"prod_dimens":  "",
+		"prod_price":  {
+	 						"price1":"215000đ",
+	 						"price2":"672000đ",
+	 					} 
+
+	},
+	{
+		"prod_id":3,
+		"prod_name":"Đèn chiếu sáng 1",
+		"prod_brand":"TLC",
+	 	"prod_power": {
+	 						"power1":"11w",
+	 						"power2":"18w",
+	 					},
+		"prod_volt": "175 ~ 240 VAC - 50/60 Hz",
+		"prod_colortemp": "",
+		"prod_lum_flux": "1100/1650 lumen",
+		"prod_dimens":  "(ØxH) D86*H53/D106*H68/D136*H80 mm",
+		"prod_price":  {
+	 						"price1":"295000đ",
+	 						"price2":"425000đ",
+	 					} 
+
+	}
+	
+]
