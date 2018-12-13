68e6d6b573e0620b1a6f4cc9273fe841e7c3d19d
Đặng Ngọc Phong <43205832+phongdn89@users.noreply.github.com>
2018-12-14 01:58:55 +0700

    Update DB

--- other/arrayDocument.js
+++ other/arrayDocument.js
@@ -1,59 +1,59 @@
-[
+[	// Mảng các sản phẩm : Products
 	{
-		"prod_id":1,
-		"prod_name":"Đèn âm trần Philips chiếu điểm 5w, 12w, 18w chip COB",
-		"prod_brand":"Philips",
-	 	"prod_power": {
-	 						"power1":"5w",
-	 						"power2":"12w",
-	 						"power3":"18w",
-	 					},
-		"prod_volt": "175 ~ 240 VAC - 50/60 Hz",
-		"prod_colortemp": "3000K|4000K|6500K",
-		"prod_lum_flux": "450/1100/1650 lumen",
-		"prod_dimens":  "(ØxH) D86*H53/D106*H68/D136*H80 mm",
-		"prod_price":  {
-	 						"price1":"158000đ",
-	 						"price2":"395000đ",
-	 						"price3":"525000đ",
-	 					} 
+		"prod_id"		: 	1,
+		"prod_name"		: 	"Đèn âm trần Philips chiếu điểm chip COB",
+		"prod_brand"	: 	"Philips",
+		"prod_category"	: 	"Đèn âm trần",
+	 	"prod_power"	:  	"12w",
+		"prod_volt"		:  	"175 ~ 240 VAC - 50/60 Hz",
+		"prod_colortemp":  	"3000K|4000K|6500K",
+		"prod_lum_flux"	:  	"450/1100/1650 lumen",
+		"prod_dimens"	:   "(ØxH) D86*H53/D106*H68/D136*H80 mm",
+		"prod_price"	:   "158000",
+		"prod_thumb"	:   "link ảnh.................", 			//Link ảnh thu nhỏ đây nhé Lan Anh
+		"prod_galerry"	:  	["link1","link2","link3"....],			//Link 3 ảnh chi tiết
+		"prod_desc"		:  	"Mô tả",
+		"brand_id"		: ,
+		"category_id"	: ,
+		"cart_id"		: ,
+		"user_id"		: ,
 
 	},
-	{
-		"prod_id":2,
-		"prod_name":"Đèn âm trần Kosoom thân nhựa, 5W - 18W",
-		"prod_brand":"Kosoom",
-	 	"prod_power": {
-	 						"power1":"5w",
-	 						"power2":"18w",
-	 					},
-		"prod_volt": "175 ~ 240 VAC - 50/60 Hz",
-		"prod_colortemp": "3000K|4000K|6500K",
-		"prod_lum_flux": "450/1100/1650 lumen",
-		"prod_dimens":  "",
-		"prod_price":  {
-	 						"price1":"215000đ",
-	 						"price2":"672000đ",
-	 					} 
+		
+]
 
-	},
+[	// Mảng các Danh mục : Categories
 	{
-		"prod_id":3,
-		"prod_name":"Đèn chiếu sáng 1",
-		"prod_brand":"TLC",
-	 	"prod_power": {
-	 						"power1":"11w",
-	 						"power2":"18w",
-	 					},
-		"prod_volt": "175 ~ 240 VAC - 50/60 Hz",
-		"prod_colortemp": "",
-		"prod_lum_flux": "1100/1650 lumen",
-		"prod_dimens":  "(ØxH) D86*H53/D106*H68/D136*H80 mm",
-		"prod_price":  {
-	 						"price1":"295000đ",
-	 						"price2":"425000đ",
-	 					} 
-
+		"category_id":1,
+		"category_name":"Đèn âm trần"
 	}
-	
+]
+
+[	// Mảng các User : Users
+	{
+	    "user_id" : 1,
+	    "username" : "namtrung",
+	    "password" : "namtrung",
+	    "fullname" : "Nam Trung",
+	    "address" : "",
+	    "tel" : "0981234567",
+	    "type" : 0										//1 là admin 2 là customs
+	}
+]
+
+[	// Mảng các Giỏ hàng : Carts
+	{
+		"cart_id":1,
+		"prod_id":1,
+		"user_id":1,
+	}
+]
+
+
+[	// Mảng các Comment : Comments
+	{
+		"cmt_id":1,
+		"cmt_content":"Very good!",
+		"prod_id":1,
+	}
 ]
